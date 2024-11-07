beaccf72ac5a9ea2a9029cf80168714244c0fd58
Prizm Moon <prdigitalmoon@gmail.com>
2024-11-07 01:15:32 -0500

    Adds active record code to bird class

--- private/classes/bird.class.php
+++ private/classes/bird.class.php
@@ -3,6 +3,15 @@
 class Bird
 {
 
+  // ------------ START OF ACTIVE RECORD CODE ------------
+  static public $database;
+
+  static public function set_database($database)
+  {
+    self::$database = $database;
+  }
+  // ------------ END OF ACTIVE RECORD CODE ------------  
+
   public $common_name;
   public $habitat;
   public $food;
